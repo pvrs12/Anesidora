f23ed28e4c50404ac971f57e11505fe87546c833
alten.r@gmail.com <alten.r@gmail.com@3da485a5-9346-de8b-2517-2f4f6fa10643>
2012-05-18 19:09:35 +0000

    1.6.2 For real.
    git-svn-id: http://pandora-extension.googlecode.com/svn/trunk@9 3da485a5-9346-de8b-2517-2f4f6fa10643

--- json/js/facebook.js
+++ json/js/facebook.js
@@ -1,10 +1,6 @@
-$('#fbCanDie').bind('load', function(){
-    getAccessToken($(this).attr('src'));
-}
 chrome.tabs.onUpdated.addListener(
     function onTabUpdated(tabId, changeInfo, tab) {
         if (changeInfo.url && changeInfo.url.indexOf('https://www.facebook.com/connect/login_success.html') == 0) {
-<<<<<<< .mine
             var hashSplit = changeInfo.url.split('#');
             if (hashSplit.length > 1) {
                 var paramsArray = hashSplit[1].split('&');
@@ -23,35 +19,8 @@ chrome.tabs.onUpdated.addListener(
                             });
                         }
                         chrome.tabs.remove(tabId);
-=======
-            getAccessToken(changeInfo.url);
-            if (localStorage.facebookId == "") {
-                $.ajax({
-                    async: false,
-                    url: "https://graph.facebook.com/" + localStorage.facebookUsername + "?" + localStorage.accessToken,
-                    dataType: "json",
-                    success: function (data) {
-                        localStorage.facebookId = data.id;
->>>>>>> .r6
                     }
-                });
+                }
             }
-            chrome.tabs.remove(tabId);
         }
     });
-function getAccessToken(url) {
-    var hashSplit = changeInfo.url.split('#');
-    if (hashSplit.length > 1) {
-        var paramsArray = hashSplit[1].split('&');
-        for (var i = 0; i < paramsArray.length; i++) {
-            var paramTuple = paramsArray[i].split('=');
-            if (paramTuple.length > 1 && paramTuple[0] == 'access_token') {
-                localStorage.accessToken = paramTuple[1];
-                
-            }
-        }
-    }
-}
-                        
-
- 
