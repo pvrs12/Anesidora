5e77aa47cce58b6f2f9a051d2fa91aaf645ad472
pvrs12 <pvrs12@gmail.com>
2017-05-10 10:32:13 -0400

    moved chrome/firefox manifest files out of the addon package

--- package.sh
+++ package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd json
-cp manifest.json.firefox manifest.json
+cp ../manifest.json.firefox manifest.json
 jar -cMf ../anesidora.xpi *
-cp manifest.json.chrome manifest.json
+cp ../manifest.json.chrome manifest.json
 jar -cMf ../anesidora.zip *
 
