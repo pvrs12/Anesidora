c69b87cd6ac497c501619966c3003f576be9f3d0
pvrs12 <pvrs12@gmail.com>
2017-09-23 20:49:32 -0400

    Removed all LastFM, Facebook, and Hotkey elements from the code. Cleaned several files' formatting

--- json/js/options.js
+++ json/js/options.js
@@ -8,22 +8,11 @@ var default_history=5;
 
 $(document).ready(function () {
 	var background = chrome.extension.getBackgroundPage();
-	$('#lastFmStatus').hide();
-	$('#lastFmLogin').hide();
 	if (localStorage.notifications == "true") {
 		$('#notifications').attr('checked', true);
 	}
 	if (localStorage.autostations == "true") {
 		$('#autostations').attr('checked', true);
-	}
-	if (localStorage.lastFm == "true") {
-		$('#lastFmToggle').attr('checked', true);
-		if (localStorage.lastFmSession) {
-			$('#lastFmStatus').text("Scrobbling as " + localStorage.lastFmUsername).show();
-		}
-		else {
-			$('#lastFmLogin').show();
-		}
 	}
 
 	initBodySize();
@@ -79,69 +68,6 @@ $(document).ready(function () {
 			localStorage.autostations = false;
 		}
 	});
-	$('#lastFmToggle').bind('change', function () {
-		if ($(this).attr('checked')) {
-			localStorage.lastFm = true;
-			if (localStorage.lastFmSession) {
-				$('#lastFmStatus').text("Scrobbling as " + localStorage.lastFmUsername).show();
-			}
-			else {
-				$('#lastFmLogin').show();
-			}
-		}
-		else {
-			localStorage.lastFm = false;
-			$('#lastFmStatus').hide();
-			$('#lastFmLogin').hide();
-		}
-	});
-	$('#lastFmForm').bind('submit', function () {
-		background.lastFmSession($('#lastFmUsername').val(), $('#lastFmPassword').val());
-		if (localStorage.lastFmSession) {
-			$('#lastFmLogin').hide();
-			$('#lastFmStatus').text("Scrobbling as " + localStorage.lastFmUsername).show();
-		}
-		else {
-			$('#lastFmLogin').show();
-		}
-		return false;
-	});
-	$('#facebookForm').bind('submit', function () {
-		localStorage.facebookUsername = $('#facebookUsername').val();
-		window.open("https://www.facebook.com/dialog/oauth?client_id=124332377700986&response_type=token&scope=publish_stream,offline_access&redirect_uri=https://www.facebook.com/connect/login_success.html");
-	});
-
-
-	// Setup Play HotKey Option
-	if (localStorage.hotkeyPlay) LoadHotKeyTextValue($('#play'), JSON.parse(localStorage.hotkeyPlay));
-	$('#play').bind('keydown', function (e) {
-		localStorage.hotkeyPlay = SetHotKey($('#play'), e);
-	});
-
-	// Setup Skip HotKey Option
-	if (localStorage.hotkeySkip) LoadHotKeyTextValue($('#skip'), JSON.parse(localStorage.hotkeySkip));
-	$('#skip').bind('keydown', function (e) {
-		localStorage.hotkeySkip = SetHotKey($('#skip'), e);
-	});
-
-	// Setup Like HotKey Option
-	if (localStorage.hotkeyLike) LoadHotKeyTextValue($('#like'), JSON.parse(localStorage.hotkeyLike));
-	$('#like').bind('keydown', function (e) {
-		localStorage.hotkeyLike = SetHotKey($('#like'), e);
-	});
-
-	// Setup DisLike HotKey Option
-	if (localStorage.hotkeyDisLike) LoadHotKeyTextValue($('#dislike'), JSON.parse(localStorage.hotkeyDisLike));
-	$('#dislike').bind('keydown', function (e) {
-		localStorage.hotkeyDisLike = SetHotKey($('#dislike'), e);
-	});
-
-	// Setup tired HotKey Option
-	if (localStorage.hotkeyTired) LoadHotKeyTextValue($('#tired'), JSON.parse(localStorage.hotkeyTired));
-	$('#tired').bind('keydown', function (e) {
-		localStorage.hotkeyTired = SetHotKey($('#tired'), e);
-	});
-
 });
 
 function initBodySize(){
@@ -164,43 +90,3 @@ function initBodySize(){
 
 }
 
-function LoadHotKeyTextValue(inputBox, keyCodes) {
-	if (!keyCodes) return;
-	var text = "";
-	if (keyCodes.ctrl) {
-		text += "Ctrl + ";
-	}
-	if (keyCodes.alt) {
-		text += "Alt + ";
-	}
-	if (keyCodes.shift) {
-		text += "Shift + ";
-	}
-	text += String.fromCharCode(keyCodes.code);
-	inputBox.val(text);
-}
-
-function IsValidKeyCode(keyCode) {
-	if (keyCode == 0 || keyCode == 16 || keyCode == 17 || keyCode == 18) return false;
-	return true;
-}
-
-function SetHotKey(inputBox, keyCodes) {
-	keyCodes.preventDefault();
-	if (!IsValidKeyCode(keyCodes.keyCode)) return "";
-
-	var text = "";
-	if (keyCodes.ctrlKey) {
-		text += "Ctrl + ";
-	}
-	if (keyCodes.altKey) {
-		text += "Alt + ";
-	}
-	if (keyCodes.shiftKey) {
-		text += "Shift + ";
-	}
-	text += String.fromCharCode(keyCodes.keyCode);
-	inputBox.val(text);
-	
-	return JSON.stringify({ ctrl: keyCodes.ctrlKey, alt: keyCodes.altKey, shift: keyCodes.shiftKey, code: keyCodes.keyCode });
-}
