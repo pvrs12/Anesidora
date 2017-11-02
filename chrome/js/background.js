fa615b0bce4200753839949da1373fbce4ceeef3
pvrs12 <pvrs12@gmail.com>
2017-11-02 13:26:54 -0400

    Moved differences between chrome and firefox to separate folders. Moved common assets to a new directory (from json=>common). Updated package.sh script to handle the new system

---
+++ chrome/js/background.js
@@ -0,0 +1,148 @@
+var callback;
+var currentSong;
+var comingSong;
+var prevSongs = new Array();
+var errorCount = 0;
+
+$(document).ready(
+function () {
+    if (localStorage.volume) {
+        mp3Player.volume = localStorage.volume;
+    }
+    else {
+        mp3Player.volume = .1;
+    }
+    $('#mp3Player')
+    .bind('play', function () {
+        try{
+            //check if the window exists
+            String($('#mp3Player'))
+            callback.updatePlayer();
+            currentSong.startTime = Math.round(new Date().getTime() / 1000);
+        } catch(e){
+            //if it doesn't, don't draw here
+            return;
+        }
+    })
+    .bind('ended', function () {
+        if (currentSong.songRating != '1') {
+            prevSongs.push(currentSong);
+            //console.log('History Num = '+localStorage.historyNum);
+            while(prevSongs.length > localStorage.historyNum){
+                prevSongs.shift();
+            }
+        }
+        errorCount = 0;
+        nextSong();
+    })
+    .bind('timeupdate', function () {
+        try {
+            //check if the window exists
+            String($('#mp3Player'))
+            callback.drawPlayer();
+        } catch(e){
+            //if it doesn't, don't draw here
+            return;
+        }
+    })
+    .bind('error', function (err) {
+        //console.log(err);
+        //if (errorCount > 3) {
+            //alert("There seems to be an issue with Anesidora. To prevent Pandora account lockout Anesidora has been stopped.");
+            //return;
+        //}
+        errorCount++;
+    })
+});
+
+if (localStorage.username != '' && localStorage.password != '') {
+    partnerLogin();
+}
+
+function setCallbacks(updatePlayer,drawPlayer,downloadSong){
+	callback = {
+        "updatePlayer": updatePlayer,
+        "drawPlayer": drawPlayer,
+        "downloadSong": downloadSong
+    };
+}
+
+function play(stationToken) {
+    if (stationToken != localStorage.lastStation) {
+        currentSong = undefined;
+        getPlaylist(stationToken);
+        localStorage.lastStation = stationToken;
+        nextSong();
+    } else {
+        if (currentSong == undefined) {
+            getPlaylist(localStorage.lastStation);
+        }
+        if (document.getElementById("mp3Player").currentTime > 0) {
+            mp3Player.play();
+        } else {
+            nextSong();
+        }
+    }
+}
+
+function nextSong() {
+    if (currentPlaylist == undefined) {
+        getPlaylist(localSTorage.lastStation);
+    }
+    if (currentSong == undefined) {
+        while (currentSong == undefined) {
+            currentSong = currentPlaylist.shift();
+        }
+    }
+    else {
+        currentSong = comingSong;
+    }
+    if (currentPlaylist.length == 0) {
+        getPlaylist(localStorage.lastStation);
+    }
+    comingSong = currentPlaylist.shift();
+
+    if (localStorage.notifications == "true") {
+        var options = {
+            type: "list",
+            title: "Now playing:\r\n" + currentSong.artistName + " - " + currentSong.songName,
+            message: "by " + currentSong.artistName,
+            eventTime: 5000,
+            items: [
+                { title: "", message: "Coming next: " },
+                { title: "", message: comingSong.artistName + " - " + comingSong.songName }
+            ]
+        }
+
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", currentSong.albumArtUrl);
+        xhr.responseType = "blob";
+        xhr.onload = function(){
+            var blob = this.response;
+            options.iconUrl = window.URL.createObjectURL(blob);
+        };
+        xhr.send(null);
+    }
+    if (currentSong.additionalAudioUrl != null) {
+        mp3Player.setAttribute("src", currentSong.additionalAudioUrl);
+    }
+    else {
+        mp3Player.setAttribute("src", currentSong.audioUrlMap.highQuality.audioUrl);
+    }
+    mp3Player.play();
+}
+
+function downloadSong() {
+	var url='';
+	if (currentSong.additionalAudioUrl != null) {
+		console.log('Downloading alternate url');
+		console.log(currentSong);
+		url=currentSong.additionalAudioUrl;
+	}	else {
+		console.log('Downloading normal url');
+		console.log(currentSong);
+		url=currentSong.audioUrlMap.highQuality.audioUrl;
+	}
+	callback.downloadSong(url,currentSong.songName);
+}
+
